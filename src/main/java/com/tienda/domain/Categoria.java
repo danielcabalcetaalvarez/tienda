051234d928a67f458838ddc011b95e3292b89e62
dcabalceta <dcabalceta.alvarez@gmail.com>
2026-02-16 20:55:32 -0600

    commit #20 con los cambios hechos en semana 6

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.io.Serializable;
+import java.util.List;
 
 import lombok.Data;
 
@@ -38,4 +40,7 @@ public class Categoria implements Serializable {
     @Column(name = "activo")
     private Boolean activo;
 
+    //Relación de uno a muchos con la clase Producto
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
 }
